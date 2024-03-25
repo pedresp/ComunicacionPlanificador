1cfde9ba0b88593b841a6a26b415069ed5c08bfa
Iker <ikersampedrosanchez@gmail.com>
2024-03-25 13:07:43 +0100

    van bien las colas

--- planificador/planificador/service.py
+++ planificador/planificador/service.py
@@ -14,10 +14,10 @@ service_active = True
 drones = []
 wps_metadata = WPS_metadata()
 
-class MinimalService(Node):
+class Station(Node):
 
     def __init__(self):
-        super().__init__('minimal_service')
+        super().__init__('Station')
         
         global i, flight_height
         i = self.declare_parameter('drones_quantity', 0.0).get_parameter_value().double_value
@@ -26,18 +26,40 @@ class MinimalService(Node):
 
     def add_two_ints_callback(self, request, response):
         global service_active, drones, i, wps_metadata
-
-        response.response = request.tof + request.speed
-        self.get_logger().info('Incoming request\ndrone_id: %s speed: %d tof: %d ancho_de_barrido: %d\ncoordx: %d, coordy: %d' % (request.drone_id, \
-                        request.speed, request.tof, request.ancho_de_barrido, request.coordx, request.coordy))
         
-        drones.append([request.coordx, request.coordy, request.ancho_de_barrido, request.speed, request.tof])
-        wps_metadata.add_drone(request.drone_id, request.ancho_de_barrido)
+        if service_active:
+            response.response = 1.0
+            self.get_logger().info('Incoming request\ndrone_id: %s speed: %d tof: %d ancho_de_barrido: %d\ncoordx: %d, coordy: %d' % (request.drone_id, \
+                            request.speed, request.tof, request.ancho_de_barrido, request.coordx, request.coordy))
+            
+            drones.append([request.coordx, request.coordy, request.ancho_de_barrido, request.speed, request.tof])
+            wps_metadata.add_drone(request.drone_id, request.ancho_de_barrido)
 
-        i = i-1
-        if i == 0:
-            service_active = False
+            i = i-1
+            if i == 0:
+                service_active = False
+                self.publish_wps()
+            return response
+        response.response = 0.0
         return response
+    
+    def publish_wps(self):
+        global drones, wps_metadata, flight_height
+        wps = verdugo(drones, os.path.join(os.getcwd(), 'install/planificador/share/planificador/config/perimeter.yaml'))
+
+        index = 0
+
+        drones_names = wps_metadata.flatten()
+        proccesed_wps = process_wps(wps, flight_height)
+
+        for list_array_2d in proccesed_wps:
+            for array_2d in list_array_2d:
+                publisher = self.create_publisher(Waypoints, f'{drones_names[index]}_wps', 10)
+                msg = Waypoints()
+                msg.wps = array_2d.flatten().tolist()
+                publisher.publish(msg)
+                self.get_logger().info('Publishing: "%s"' % drones_names[index])
+                index = index + 1
 
 """
 class MinimalClient(Node):
@@ -78,30 +100,9 @@ class MinimalPublisher(Node):
 def main():
     rclpy.init()
 
-    minimal_service = MinimalService()
+    station = Station()
 
-    while service_active:
-        rclpy.spin_once(minimal_service)
-
-    wps = verdugo(drones, os.path.join(os.getcwd(), 'install/planificador/share/planificador/config/perimeter.yaml'))
-    print(drones)
-
-    minimal_service.destroy_node()
-
-    index = 0
-    print("waypoints")
-    print(wps)
-
-    global flight_height
-
-    drones_names = wps_metadata.flatten()
-    proccesed_wps = process_wps(wps, flight_height)
-
-    for list_array_2d in proccesed_wps:
-        for array_2d in list_array_2d:
-            minimal_publisher = MinimalPublisher(drones_names[index], array_2d)
-            minimal_publisher.destroy_node()
-            index = index + 1
+    rclpy.spin(station)
 
     rclpy.shutdown()
 
