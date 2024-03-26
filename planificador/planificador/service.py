8ac792c64e5c706439809d6a74eb7e1f8774bf6a
Iker <ikersampedrosanchez@gmail.com>
2024-03-26 13:08:04 +0100

    integracion de colas con simplesim

--- planificador/planificador/service.py
+++ planificador/planificador/service.py
@@ -22,7 +22,7 @@ class Station(Node):
         global i, flight_height
         i = self.declare_parameter('drones_quantity', 0.0).get_parameter_value().double_value
         flight_height = self.declare_parameter('flight_height', 0.0).get_parameter_value().double_value
-        self.srv = self.create_service(AdvService, 'adv_service', self.add_two_ints_callback)
+        self.srv = self.create_service(AdvService, '/adv_service', self.add_two_ints_callback)
 
     def add_two_ints_callback(self, request, response):
         global service_active, drones, i, wps_metadata
@@ -33,7 +33,7 @@ class Station(Node):
                             request.speed, request.tof, request.ancho_de_barrido, request.coordx, request.coordy))
             
             drones.append([request.coordx, request.coordy, request.ancho_de_barrido, request.speed, request.tof])
-            wps_metadata.add_drone(request.drone_id, request.ancho_de_barrido)
+            wps_metadata.add_drone(Drone_initial(request.drone_id, (request.coordx, request.coordy)), request.ancho_de_barrido)
 
             i = i-1
             if i == 0:
@@ -54,9 +54,12 @@ class Station(Node):
 
         for list_array_2d in proccesed_wps:
             for array_2d in list_array_2d:
-                publisher = self.create_publisher(Waypoints, f'{drones_names[index]}_wps', 10)
+                array_to_send = np.append(np.array([[drones_names[index].coords[0], drones_names[index].coords[1], flight_height]]), \
+                                      array_2d, axis=0)
+                publisher = self.create_publisher(Waypoints, f'/{drones_names[index].name}/{drones_names[index].name}_wps', 10)
                 msg = Waypoints()
-                msg.wps = array_2d.flatten().tolist()
+                msg.wps = array_to_send.flatten().tolist()
+
                 publisher.publish(msg)
                 self.get_logger().info('Publishing: "%s"' % drones_names[index])
                 index = index + 1
